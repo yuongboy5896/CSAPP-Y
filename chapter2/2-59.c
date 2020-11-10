f946a5ef426b3e7e6922d47bd110ccea788364c9
yuongboy5896 <yang186107321@163.com>
2020-11-10 23:07:32 +0800

    edit main（）

--- chapter2/2-59.c
+++ chapter2/2-59.c
@@ -9,7 +9,7 @@ int main(int argc,char *argv[])
 {
     int x = 0x89ABCDEF;
     int y = 0x76543210;
-    int z = 0x76543210 & 0xFFFFFF00 | 0x000000FF & 0x89ABCDEF
+    int z = 0x76543210 & 0xFFFFFF00 | 0x000000FF & 0x89ABCDEF;
     printf("%x \n",z);
 
 

3484dddc3978d97664017d56a81a783a8ded5522
yuongboy5896 <yang186107321@163.com>
2020-11-10 23:04:58 +0800

    add 2-59.c

---
+++ chapter2/2-59.c
@@ -0,0 +1,17 @@
+/*  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc,char *argv[])
+{
+    int x = 0x89ABCDEF;
+    int y = 0x76543210;
+    int z = 0x76543210 & 0xFFFFFF00 | 0x000000FF & 0x89ABCDEF
+    printf("%x \n",z);
+
+
+}
+

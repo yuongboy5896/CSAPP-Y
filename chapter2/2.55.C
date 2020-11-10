0680e3a1ab0ccbf6291725ef385a361907462e3b
yuongboy5896 <yang186107321@163.com>
2020-11-10 18:00:44 +0800

    add is_little_endan

---
+++ chapter2/2.55.C
@@ -0,0 +1,35 @@
+/* show-bytes - prints byte representation of data */
+
+/* $begin show-bytes */
+#include <stdio.h>
+/* $end show-bytes */
+#include <stdlib.h>
+#include <string.h>
+/* $begin show-bytes */
+
+typedef unsigned char *  byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len){
+    size_t  i;
+    for (i = 0; i < len; i++)
+    {
+        printf("%p\t0x%.2x\n",&start[i],start[i]);
+    }
+    printf("\n");
+    
+}
+
+int is_little_endina()
+{
+    int little = 1;
+    byte_pointer start =  &little;
+    printf("%p\t0x%.2x\n",&start[0],start[0]);
+    return &start[0];
+} 
+
+
+int main(int argc,char *argv[])
+{
+    is_little_endina();
+    return 0;
+}

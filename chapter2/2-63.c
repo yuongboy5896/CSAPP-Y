ae5b7a53d1798832d8a176b9bc88f0caeb4e1e02
yuongboy5896 <yang186107321@163.com>
2020-12-06 23:36:34 +0800

    update 2_63.c

--- chapter2/2-63.c
+++ chapter2/2-63.c
@@ -8,32 +8,36 @@
 /* $begin srl sra  */
 
 unsigned srl(unsigned x, int k){
+	// error ~~
+	//int  shift_val = 8 * (sizeof(int)) - 1;
+	//int  tmp = 1 << shift_val >> k;
+	//int  tmp1 = (~0 - (1 << shift_val)) >>k;
+	//unsigned xsra = (int) x >> k;
+	////printf("(int) x >> k %x \n", xsra);
+	//return	xsra & 1 << shift_val >> k | x & (~0 - (1 << shift_val)) >> k;
 
-	int  shift_val = 8 * (sizeof(int)) - 1;
-	int  tmp = 1 << shift_val >> k;
-	int  tmp1 = (~0 - (1 << shift_val)) >>k;
-
-	unsigned xsra = (int) x >> k;
-
-	
-
-
-	//printf("(int) x >> k %x \n", xsra);
-	return	xsra & 1 << shift_val >> k | x & (~0 - (1 << shift_val)) >> k;
+	int shift_val = 8 * (sizeof(int)) - i;
+	unsigned xsra = (int)x >> k;
+	unsigned tmp = ~( -1 << (shift_val - k));
+	return xsra & tmp;
 
 }
 
 int sra(int x, int k){
+	// error
+	//int xsrl = (unsigned)x >> k;
+	//int  shift_val = 8 * (sizeof(int)) - 1;
+	//int  tmp1 = (~0 - (1 << shift_val)) >> k;
+	//int tmp2 = x & tmp1;
+	//int  tmp = xsrl&(unsigned)1 << shift_val >> k;
+	//printf("sra：%x \n", tmp | tmp2);
+	//return xsrl & 1 << shift_val >> k |x & ~0 - (1 << shift_val) >> k;
+
 	int xsrl = (unsigned)x >> k;
-	int  shift_val = 8 * (sizeof(int)) - 1;
-	int  tmp1 = (~0 - (1 << shift_val)) >> k;
+	int shift_val = 8 * (sizeof(int)) - 1;
+	int mask = x >> shift_val << shift_val >> k;
+	return xsrl | mask;
 
-	int tmp2 = x & tmp1;
-	int  tmp = xsrl&(unsigned)1 << shift_val >> k;
-	 
-	printf("sra：%x \n", tmp | tmp2);
-
-	return xsrl & 1 << shift_val >> k |x & ~0 - (1 << shift_val) >> k;
 	
 }
 
